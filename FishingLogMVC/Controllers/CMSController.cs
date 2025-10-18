4c1558c76005141a23b4c9102a223596a9ec4f93
my9281 <t5049477346@gmail.com>
2025-10-19 04:54:46 +0800

    submit

---
+++ FishingLogMVC/Controllers/CMSController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FishingLogMVC.Controllers
+{
+    public class CMSController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
